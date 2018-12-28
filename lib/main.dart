7b75e1b59f8fbd749508d57c64ee76c842aa8554
faker <mr.bes1996@mail.ru>
2018-12-28 22:11:57 +0300

    Init commit with start page

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import './themes.dart';
+import './start_page.dart';
+
+void main() {
+  runApp(HonestTestApp());
+}
+
+class HonestTestApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Faker chat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: StartPage(),
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/login':
+            return MaterialPageRoute(
+              builder: (_) => StartPage(),
+              settings: settings,
+            );
+
+          case '/questions':
+            return MaterialPageRoute(
+              builder: (_) => StartPage(),
+              settings: settings,
+            );
+        }
+      },
+    );
+  }
+}
---
+++ lib/start_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(StartPage());
+
+class StartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                stops: [0.1, 0.5, 0.7, 0.9],
+                colors: [
+                  Colors.indigo[800],
+                  Colors.red[700],
+                  Colors.indigo[600],
+                  Colors.indigo[500],
+                ],
+                tileMode: TileMode.repeated),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 80.0),
+              ),
+              Image.asset(
+                'assets/logo.jpg',
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 60.0),
+              ),
+              ButtonTheme(
+                minWidth: 250.0,
+                height: 50.0,
+                child: OutlineButton(
+                  color: Colors.green,
+                  // splashColor: Colors.teal,
+                  textColor: Colors.white,
+                  child: Text(
+                    'Lets begin',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/questions');
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
